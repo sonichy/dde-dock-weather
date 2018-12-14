2607b1586fa33b5da006cbee97eb3bb097ab8046
sonichy <sonichy@163.com>
2018-12-14 16:18:54 +0800

    DDE-Dock API 1.0 -> 1.1

--- weatherplugin.cpp
+++ weatherplugin.cpp
@@ -164,7 +164,7 @@ void WeatherPlugin::invokedMenuItem(const QString &itemKey, const QString &menuI
 
 void WeatherPlugin::MBAbout()
 {
-    QMessageBox aboutMB(QMessageBox::NoIcon, "海天鹰天气预报 4.9.1", "关于\n\n深度Linux系统上一款在任务栏显示天气的插件。\n作者：黄颖\nE-mail: sonichy@163.com\n源码：https://github.com/sonichy/WEATHER_DDE_DOCK\n天气API：https://www.sojson.com/blog/305.html");
+    QMessageBox aboutMB(QMessageBox::NoIcon, "海天鹰天气预报 4.9.2", "关于\n\n深度Linux系统上一款在任务栏显示天气的插件。\n作者：黄颖\nE-mail: sonichy@163.com\n源码：https://github.com/sonichy/WEATHER_DDE_DOCK\n天气API：https://www.sojson.com/blog/305.html");
     aboutMB.setIconPixmap(QPixmap(":/icon/sunny.png"));
     aboutMB.exec();
 }
